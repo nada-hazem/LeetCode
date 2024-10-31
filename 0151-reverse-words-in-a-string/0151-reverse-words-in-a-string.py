bba789bf2763203971be4b32181b0312f6e3cd40
Nada Hazem Ayoub <74680843+nada-hazem@users.noreply.github.com>
2024-10-31 15:46:18 +0300

    Time: 0 ms (100.00%), Space: 16.6 MB (69.37%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         
-        words = s.split()
+        words = s.split() 
         i=0
         j=len(words)-1
         
@@ -10,7 +10,21 @@ class Solution:
             i+=1
             j-=1
         return " " . join(words)
+    
+    
+  #another solution  
+
+
+        #class Solution:
+        #def reverseWords(self, s: str) -> str:
         
+        #words = s.split()
+        
+   
+        #words.reverse()
+        
+       
+        #return " ".join(words)
         
        
             
